17d0683eb448c44ebfb82266e1a36b507d859c07
Romulo Augusto <romulomail@gmail.com>
2020-03-19 08:26:22 -0300

    criação do projeto - infraestrutura angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  private title = "Princípios da permacultura";
+  private url = "https://permacultura.paginas.ufsc.br/files/2019/08/principios-1.png";
+  private description = "Princípios da permacultura";
+}
