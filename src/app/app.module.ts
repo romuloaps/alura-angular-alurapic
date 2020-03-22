c7a77c09a5d57de9f48de4a0248c84d2a7781daa
Romulo Augusto <romulomail@gmail.com>
2020-03-22 00:59:08 -0300

    importa o HttpClient no módulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 
 import { PhotosModule } from './photos/photos.module';
@@ -12,6 +13,7 @@ import { PhotosModule } from './photos/photos.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     PhotosModule
   ],
   providers: [],
