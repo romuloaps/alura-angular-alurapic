21e29e4b1ea46924c304900291728dae7c133e44
Romulo Augusto <romulomail@gmail.com>
2020-03-24 06:57:50 -0300

    cria um novo component para listar as fotos; remove o uso do photo.component do app.component; encapsula tudo dentro de photo.module, inclusive o CommonModule para uso de diretivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { PhotosModule } from './photos/photos.module';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    PhotosModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
