c17aee9c9a77bc3e5a69c4fd332d7fe17da0229d
Romulo Augusto <romulomail@gmail.com>
2020-03-25 06:55:08 -0300

    define um parâmetro para a URL de fotos do usuário; usa o parâmetro no component de listagem de fotos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
@@ -12,11 +13,14 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
   
-  constructor(private photoService: PhotoService) { }
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
     this.photoService 
-      .listFromUser("flavio")
+      .listFromUser(this.activatedRoute.snapshot.params.userName)
       .subscribe(photos => this.photos = photos);
   }
 }
