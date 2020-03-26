f2f091c20270173c61fc0a508f50152f54b11312
Romulo Augusto <romulomail@gmail.com>
2020-03-26 07:41:56 -0300

    cria um "resolver" para carregar a lista de fotos antes da criação do componente, evitando criar o componente com a lista vazia mesmo tendo conteúdo

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-list',
@@ -14,14 +13,9 @@ export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter: string = "";
   
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.photoService 
-      .listFromUser(this.activatedRoute.snapshot.params.userName)
-      .subscribe(photos => this.photos = photos);
+    this.photos = this.activatedRoute.snapshot.data.photos;
   }
 }
---
+++ src/app/photos/photo-list/photo-list.resolver.ts
@@ -0,0 +1,19 @@
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: "root"
+})
+export class PhotoListResolver implements Resolve<Observable<Photo[]>> {
+
+    constructor(private service: PhotoService) {}
+    
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Photo[]> {
+        const userName = route.params.userName;
+        return this.service.listFromUser(userName);
+    }
+}
