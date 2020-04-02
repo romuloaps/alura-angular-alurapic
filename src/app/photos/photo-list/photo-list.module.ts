8344069cd2086534073d9e76b97b1048e0854eff
Romulo Augusto <romulomail@gmail.com>
2020-04-02 07:05:50 -0300

    componentiza o campo de busca de fotos; cria comunicação entre elementos pai/filho

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -7,13 +7,15 @@ import { LoadButtonComponent } from './load-button/load-button.component';
 import { PhotosBootstrapRenderComponent } from './photos-bootstrap-render/photos-bootstrap-render.component';
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from '../../shared/components/card/card.module';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
     declarations:[
         PhotoListComponent,
         PhotosBootstrapRenderComponent,
         LoadButtonComponent,
-        FilterByDescription
+        FilterByDescription,
+        SearchComponent
     ],
     imports: [
         CommonModule,
