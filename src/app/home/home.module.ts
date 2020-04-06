dff1dee4acbcaa2be35f2bdfe1c349e1ae2a0f42
Romulo Augusto <romulomail@gmail.com>
2020-04-06 06:45:17 -0300

    componentiza a mensagem de validação e usa no signin

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { SigninComponent } from './signin/signin.component';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
   declarations: [SigninComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    VMessageModule
   ]
 })
 export class HomeModule { }
---
+++ src/app/shared/components/vmessage/vmessage.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ap-vmessage',
+  templateUrl: './vmessage.component.html',
+  styleUrls: ['./vmessage.component.css']
+})
+export class VMessageComponent {
+
+  @Input() text: string = "";
+}
