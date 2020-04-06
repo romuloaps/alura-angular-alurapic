3fca9b10eaec1c4023138ee420b8efda98466a59
Romulo Augusto <romulomail@gmail.com>
2020-04-06 07:06:22 -0300

    usa o serviço de autenticação no login (wip)

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { AuthService } from '../../core/auth/auth.service';
+
 @Component({
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
@@ -9,12 +11,30 @@ export class SigninComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       userName: ["", Validators.required],
       password: ["", Validators.required]
     });
+  }
+
+  login(): void {
+    const userName = this.loginForm.get("userName").value;
+    const password = this.loginForm.get("password").value;
+
+    this.authService
+      .authenticate(userName, password)
+      .subscribe(
+        () => console.log("autenticado!"),
+        error => {
+          console.log(error);
+          this.loginForm.reset();
+        }
+      )
   }
 }
