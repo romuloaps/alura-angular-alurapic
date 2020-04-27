d1ae862eb366378170355f33c151b5ec16756e38
Romulo Augusto <romulomail@gmail.com>
2020-04-27 07:51:33 -0300

    cria o componente Footer com os botões para home e para add

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,14 +5,21 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './auth/auth.interceptor';
 
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-    declarations: [HeaderComponent],
+    declarations: [
+        HeaderComponent,
+        FooterComponent
+    ],
     imports: [
         CommonModule,
         RouterModule
     ],
-    exports: [HeaderComponent],
+    exports: [
+        HeaderComponent,
+        FooterComponent
+    ],
     providers: [
         {
             provide: HTTP_INTERCEPTORS,
