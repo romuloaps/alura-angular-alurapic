af601904a48be0c47290775c8206c847e78fcfc1
Romulo Augusto <romulomail@gmail.com>
2020-04-27 08:15:00 -0300

    cria o form de upload com validação dos campos

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { PhotoFormComponent } from './photo-form.component';
+import { VMessageModule } from '../../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [
         PhotoFormComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule
     ]
 })
 export class PhotoFormModule { }
