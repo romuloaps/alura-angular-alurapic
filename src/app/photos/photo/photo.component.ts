12c5f0ad69f099c4fb209aef5a745ac3414575a8
Romulo Augusto <romulomail@gmail.com>
2020-04-29 07:47:27 -0300

    ajusta o componente de foto para mostrar imagens em base64 e do cloud

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ApiUtils } from '../../core/utils/api-utils';
 
 @Component({
     selector: "ap-photo",
@@ -7,6 +8,20 @@ import { Component, Input } from '@angular/core';
 })
 export class PhotoComponent {
 
-    @Input() url = "";
+    private _url: string;
+
     @Input() description = "";
+    
+    @Input() set url(url: string) {
+        if (!url.startsWith("data")) {
+            this._url = `${ApiUtils.API_URL_PREFIX}/imgs/${url}`;
+        
+        } else {
+            this._url = url;
+        }
+    }
+
+    get url(): string {
+        return this._url;
+    }
 }
