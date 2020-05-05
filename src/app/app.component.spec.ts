3c8de54bb69cb3fdaa9ca480e6e47f458813fd26
Romulo Augusto <romulomail@gmail.com>
2020-05-05 07:44:54 -0300

    remove erros do teste do AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'alura-angular-alurapic'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('alura-angular-alurapic');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('alura-angular-alurapic app is running!');
-  });
 });
