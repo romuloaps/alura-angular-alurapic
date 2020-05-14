004e14c9957b9dc87de0603f82f6b09c5574fb6f
Romulo Augusto <romulomail@gmail.com>
2020-05-14 07:52:37 -0300

    teste do AuthService usando spy do UserService e mock do HttpClient

--- src/app/core/auth/auth.service.spec.ts
+++ src/app/core/auth/auth.service.spec.ts
@@ -1,24 +1,53 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
 
 describe("AuthService", () => {
 
     let service: AuthService;
+    let httpMock: HttpTestingController;
+    let userService: UserService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [HttpClientModule],
+            imports: [HttpClientTestingModule],
             providers: [
                 AuthService
             ]
         });
 
         service = TestBed.get(AuthService);
+        httpMock = TestBed.get(HttpTestingController);
+        userService = TestBed.get(UserService);
     });
 
     it("deve ser instanciado", () => {
         expect(service).toBeTruthy();
     });
+
+    it("deve autenticar o usuário", () => {
+        const fakeBody = {
+            id: 1,
+            name: "Teste",
+            email: "teste@teste.com"
+        };
+
+        spyOn(userService, "setToken").and.returnValue(null);
+
+        service
+            .authenticate("teste", "123")
+            .subscribe(response => {
+                expect(response.body).toEqual(fakeBody);
+                expect(response.headers.get("x-access-token")).toBe("tokenTeste");
+            });
+
+        const request = httpMock.expectOne(req => req.method === "POST");
+        request.flush(fakeBody, {
+            headers: {
+                "x-access-token": "tokenTeste"
+            }
+        });
+    })
 });
