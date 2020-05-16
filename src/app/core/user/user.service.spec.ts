12b55deac73e9f6253121b873a52bff7fc7fa4c6
Romulo Augusto <romulomail@gmail.com>
2020-05-16 10:29:34 -0300

    instancia o TokenService como SpyObj no TestBed do UserService

--- src/app/core/user/user.service.spec.ts
+++ src/app/core/user/user.service.spec.ts
@@ -1,20 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import { TokenService } from '../token/token.service';
 
 describe("UserService", () => {
 
     const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6ImZsYXZpbyIsImVtYWlsIjoiZmxhdmlvQGFsdXJhcGljLmNvbS5iciIsImlhdCI6MTU4OTE5MzM2MCwiZXhwIjoxNTg5Mjc5NzYwfQ.HCJ-zTE0z_8P3Fv8w86lybKLsrEM_jFXxdBqHCSyOvc";
 
     let service: UserService;
+    let tokenService: jasmine.SpyObj<TokenService>;
 
     beforeEach(() => {
+        const tokenServiceDependencySpy = jasmine.createSpyObj("TokenService", ["getToken", "hasToken", "setToken", "removeToken"]);
+
         TestBed.configureTestingModule({
             providers: [
-                UserService
+                UserService,
+                { provide: TokenService, useValue: tokenServiceDependencySpy }
             ]
         });
-        service = TestBed.get(UserService);
+        service = TestBed.inject(UserService);
+        tokenService = TestBed.inject(TokenService) as jasmine.SpyObj<TokenService>;
+        tokenService.getToken.and.returnValue(TOKEN);
+        tokenService.setToken.and.returnValue(null);
+        tokenService.removeToken.and.returnValue(null);
     })
 
     it("deve ser instanciado", () => {
@@ -22,6 +31,8 @@ describe("UserService", () => {
     });
 
     it("deve recuperar dados do usuário através do token informado", () => {
+        tokenService.hasToken.and.returnValue(true);
+
         service.setToken(TOKEN);
 
         expect(service.isLogged()).toBeTrue();
@@ -30,6 +41,8 @@ describe("UserService", () => {
     });
 
     it("deve limpar os dados do usuário após o logout", () => {
+        tokenService.hasToken.and.returnValue(false);
+        
         service.setToken(TOKEN);
         service.logout();
 
