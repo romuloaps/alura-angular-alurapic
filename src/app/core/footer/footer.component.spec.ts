9595ac4bf3194963c3003c551315335b65794665
Romulo Augusto <romulomail@gmail.com>
2020-05-20 08:22:25 -0300

    usa a função async encapsulando o testbed e a função compileComponents para a criação dos components

--- src/app/core/footer/footer.component.spec.ts
+++ src/app/core/footer/footer.component.spec.ts
@@ -1,4 +1,5 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { FooterComponent } from './footer.component';
 import { UserService } from '../user/user.service';
@@ -7,14 +8,18 @@ describe("Footer", () => {
 
     let component: FooterComponent;
 
-    beforeEach(() => {
+    beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [FooterComponent],
             providers: [
                 { provide: UserService, useValue: jasmine.createSpyObj("UserService", ["getUser"]) }
-            ]
-        });
+            ],
+            imports: [RouterTestingModule]
+        }).compileComponents();
 
+    }));
+
+    beforeEach(() => {
         const fixture = TestBed.createComponent(FooterComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
